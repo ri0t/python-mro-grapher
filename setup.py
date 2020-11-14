64f093232fc0e30e62f72b649d08b75f4ff95afa
ri0t <riot@c-base.org>
2020-11-14 02:06:42 +0100

    updates repo name, adds license etc.

--- setup.py
+++ setup.py
@@ -1,6 +1,32 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
 
+# Python MRO-graph generator
+# ==========================
+# Copyright (C) 2003 Michele Simionato
+# Copyright (C) 2019-2020 Heiko 'riot' Weinen
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""
+Based upon Michele Simionato's work from 2003:
+http://www.phyast.pitt.edu/~micheles/python/drawMRO.html
+
+Notice the - back then - missing ternary operations.
+That was so old-school :D
+"""
+
 from setuptools import setup
 
 with open("README.rst", "r") as f:
@@ -10,20 +36,20 @@ setup(
     name="mro-graph",
     description="A small tool to generate and illustrate (via dot) the method "
                 "resolution order graph of Python objects ",
-    author="Michele Simionato ",
+    author="Michele Simionato",
     author_email="mis6@pitt.edu",
     maintainer="Heiko 'riot' Weinen",
     maintainer_email="riot@c-base.org",
     project_urls={
-        "Download": "https://github.com/ri0t/python-mro-grapher/releases",
-        "Source": "https://github.com/ri0t/python-mro-grapher",
-        "Tracker": "https://github.com/ri0t/python-mro-grapher/issues",
+        "Download": "https://github.com/ri0t/python-mro-graph/releases",
+        "Source": "https://github.com/ri0t/python-mro-graph",
+        "Tracker": "https://github.com/ri0t/python-mro-graph/issues",
     },
-    license="Python-like",  # ?!? PSF-License?
+    license="GNU General Public License v3",  # Was: PSF-like, GPL3 is PSF-like
     keywords="developer-tools mro python2 python3 method resolution order dot "
              "visualizer tool graph",
     classifiers=[
-        "Development Status :: 4 - Beta",  # Hmm.
+        "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
         "Programming Language :: Python :: 2",
         "Programming Language :: Python :: 3",
